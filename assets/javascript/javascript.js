321a6383cda59369970f84b8174e350257c83875
Olena <ryb00@i.ua>
2023-12-05 14:56:19 +0000

    Add

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,24 @@
+window.alert("hello word");
+document.write("<center><h1>Hello world</h1></center>");
+let x = 3;
+let y = 6;
+let z = 8;
+let A = x + y + z;
+let B = (2*x) + (3*z);
+console.log("A = ",A);
+console.log("B = ",B);
+window.alert(A);
+window.alert(B);
+document.write(A);
+document.write(B);
+document.write("<p>", A, "</p>");
+document.write("<p>B=", B, "</p>");
+
+fname = prompt("What's your name?");
+document.write(fname);
+
+for (counter = 1 ; counter <= 10 ; counter = counter + 1)
+{
+    document.write("<p>", counter, "  ", counter*6, "</p>");
+
+}
